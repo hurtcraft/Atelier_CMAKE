55ce4dd929561b5f8d8a903b600d8f68e3d45717
hurtcraft <abeuhkclefeuh@gmail.com>
2025-12-04 10:38:48 +0100

    fix main

--- src/main.c
+++ src/main.c
@@ -3,37 +3,53 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "square") == 0) {
-		r = _square(atof(a));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+int main(int argc, char *argv[]) {
 
-		
+    if (argc < 3) {
+        printf("Erreur de parametres");
+        return 1;
+    }
+
+    char* op = argv[1];
+
+    if (strcmp(op, "square") == 0) {
+        if (argc != 3) {
+            printf("Erreur de parametres");
+            return 1;
+        }
+
+        double a = atof(argv[2]);
+        double r = _square(a);
+        printf("%lf", r);
+        return 0;
+    }
+
+    if (argc != 4) {
+        printf("Erreur de parametres");
+        return 1;
+    }
+
+    double a = atof(argv[2]);
+    double b = atof(argv[3]);
+    double r = 0;
+
+    if (strcmp(op, "add") == 0) {
+        r = _add(a, b);
+    }
+    else if (strcmp(op, "sub") == 0) {
+        r = _sub(a, b);
+    }
+    else if (strcmp(op, "mul") == 0) {
+        r = _mul(a, b);
+    }
+    else if (strcmp(op, "div") == 0) {
+        r = _div(a, b);
+    }
+    else {
+        printf("Erreur de parametres");
+        return 1;
+    }
+
+    printf("%lf", r);
+    return 0;
+}
